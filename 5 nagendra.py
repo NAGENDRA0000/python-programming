751b09d9a94927f1f87414912c0fed58c995a9d3
NAGENDRA0000 <143808246+NAGENDRA0000@users.noreply.github.com>
2023-09-01 05:13:43 -0700

    second hours

---
+++ 5 nagendra.py
@@ -0,0 +1,20 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/GLAU/AppData/Local/Programs/Python/Python311/5 nagendra.py =
+the number of seconds1000
+number of hours 41
+number of minutes 40
+>>> 
+= RESTART: C:/Users/GLAU/AppData/Local/Programs/Python/Python311/5 nagendra.py =
+the number of seconds
+Traceback (most recent call last):
+  File "C:/Users/GLAU/AppData/Local/Programs/Python/Python311/5 nagendra.py", line 1, in <module>
+    a = int(input("the number of seconds"))
+ValueError: invalid literal for int() with base 10: ''
+>>> 
+= RESTART: C:/Users/GLAU/AppData/Local/Programs/Python/Python311/5 nagendra.py =
+the number of seconds3672
+number of hours 1
+number of minutes 1
+number of seconds 12
