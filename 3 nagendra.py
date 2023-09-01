72ffa95e04817af9ba5bceee5efd77268ee83143
NAGENDRA0000 <143808246+NAGENDRA0000@users.noreply.github.com>
2023-09-01 05:16:58 -0700

    age to vote

---
+++ 3 nagendra.py
@@ -0,0 +1,35 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/GLAU/AppData/Local/Programs/Python/Python311/nagendra.py
+number of days20
+number of weeks 2
+number of days 6
+>>> 
+== RESTART: C:/Users/GLAU/AppData/Local/Programs/Python/Python311/nagendra.py ==
+enter the temperature32
+the fahrenheit is 89.6
+the kelvin is
+>>> the kelvin is
+SyntaxError: invalid syntax
+>>> 
+== RESTART: C:/Users/GLAU/AppData/Local/Programs/Python/Python311/nagendra.py ==
+enter the temperature35
+the fahrenheit is 95.0
+the kelvin is 308
+>>> 
+= RESTART: C:/Users/GLAU/AppData/Local/Programs/Python/Python311/1 nagendra.py =
+enter the number3
+enter the number2
+the area of the triangle 3.0
+>>> 
+= RESTART: C:/Users/GLAU/AppData/Local/Programs/Python/Python311/2 nagendra.py =
+enter your age here19
+Traceback (most recent call last):
+  File "C:/Users/GLAU/AppData/Local/Programs/Python/Python311/2 nagendra.py", line 3, in <module>
+    re =age>=18
+NameError: name 'age' is not defined
+>>> 
+= RESTART: C:/Users/GLAU/AppData/Local/Programs/Python/Python311/2 nagendra.py =
+enter your age here19
+Can vote
